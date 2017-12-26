7fbaea653123e80c89eabf93f13fd95afc0f42bb
桂素伟 <axzxs2001@163.com>
2017-12-26 09:01:30 +0800

    添加Node和TaggedAddress

---
+++ ConsulSample/ConsulSharp/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsulSharp
+{
+    /// <summary>
+    /// Node
+    /// </summary>
+    public class Node1
+    {
+        public string ID { get; set; }
+
+        public string Node { get; set; }
+
+        public string Address { get; set; }
+
+        public string Datacenter { get; set; }
+
+        public TaggedAddress[] TaggedAddresses { get; set; }
+
+        public object Meta { get; set;}
+
+        public int CreateIndex { get; set; }
+
+        public int ModifyIndex { get; set; }
+    }
+}
